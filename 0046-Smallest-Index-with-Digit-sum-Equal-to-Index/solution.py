f2a37e63635c5dcf1b78d46732eabf6829a91609
Priyanshu Mamgain <mamgainp26@gmail.com>
2026-09-24 17:56:24 +0530

    Update solution for 0046

---
+++ 0046-Smallest-Index-with-Digit-sum-Equal-to-Index/solution.py
@@ -0,0 +1,16 @@
+
+from traitlets import List
+
+class Solution:
+    def smallestIndex(self, nums: List[int]) -> int:
+        for i, num in enumerate(nums):
+            digit_sum = 0
+
+            while num > 0:
+                digit_sum += num % 10
+                num //= 10
+
+            if digit_sum == i:
+                return i
+
+        return -1
